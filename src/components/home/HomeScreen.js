0cfc47043eaf73a482cf3dc2d3a541af27314ab7
Paul Cortes <paulkerg@gmail.com>
2020-11-11 00:21:48 -0500

    inclusion de emisora

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -5,6 +5,8 @@ export const HomeScreen = () => {
         <div className="container mt-2">        
             <h1>Home</h1>
             <hr />
+            <h4>Emisora</h4>
+            <audio src="https://ssl.aloncast.com:1564/" controls="true"></audio>
             {/* <HeroList publisher="Marvel Comics" /> */}
         </div>
     )
