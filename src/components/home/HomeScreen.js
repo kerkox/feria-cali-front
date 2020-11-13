e23e6f5ec1556aebe78c733a8c41fab023b5bfd6
Paul Cortes <paulkerg@gmail.com>
2020-11-13 02:13:36 -0500

    Mejoras y avances del home

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -4,18 +4,22 @@ import Carousel from '../carousel/Carousel'
 import Categories from '../categories/Categories';
 import useInitialState from '../../hooks/useInitialState';
 import './HomeScreen.scss'
+import home_1 from '../../assets/static/home_1.png'
 
 export const HomeScreen = () => {
 	const initialState = useInitialState();
 	return (
 		<div className="">
-			<section>
+			<section className="section section-1">
 				<div className="center">
 					<h1>Home</h1>
-					<hr />
-					<h4>Emisora</h4>
-					<audio src="https://ssl.aloncast.com:1564/" controls={true}></audio>
+					<img src={home_1} alt=""/>
 				</div>
+			</section>
+			<section className="section section-2">
+				<hr />
+				<h4>Emisora</h4>
+				<audio src="https://ssl.aloncast.com:1564/" controls={true}></audio>
 			</section>
 			<section>
 				<div className="full-height">
