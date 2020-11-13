c050f3c2b657652a9caf1bf4cad1525142e7facb
Paul Cortes <paulkerg@gmail.com>
2020-11-13 01:00:33 -0500

    Mejora en el navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,85 +2,81 @@ import React, { useContext } from 'react';
 import { Link, NavLink, useHistory } from 'react-router-dom';
 import { AuthContext } from '../../auth/AuthContext';
 import { types } from '../../types/types';
+import { makeStyles } from "@material-ui/core/styles"
+import { List, ListItem, ListItemText } from "@material-ui/core"
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import logo from './../../assets/static/logo_feria.png'
+import './Navbar.scss'
+
+const navLinks = [
+	{ title: `Inicio`, path: `/home` },
+	{ title: `Streaming`, path: `/streaming` },
+	{ title: `Juegos`, path: `/juegos` },	
+]
+
+const useStyles = makeStyles({
+	navDisplayFlex: {
+		display: `flex`,
+		justifyContent: `space-between`
+	},
+	linkText: {
+		textDecoration: `none`,
+		// textTransform: `uppercase`,
+		color: `white`,		
+	},
+});
 
 export const Navbar = () => {
 
-    const { user, dispatch } = useContext(AuthContext);
-    const history = useHistory();
+	const { user, dispatch } = useContext(AuthContext);
+	const history = useHistory();
 
-    const handleLogout = () => {
+	const handleLogout = () => {
 
-        history.replace('/');
+		history.replace('/');
 
-        dispatch({
-            type: types.logout
-        });
-    }
+		dispatch({
+			type: types.logout
+		});
+	}
+
+	const classes = useStyles(); 
 
 
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Feria Cali
-            </Link>
-
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/home"
-                    >
-                        Home
-                    </NavLink>
-
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/streaming"
-                    >
-                        Streaming
-                    </NavLink>
-                    
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/juegos"
-                    >
-                        Juegos
-                    </NavLink>
-                </div>
-            </div>
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
-
-                    <span className="nav-item nav-link text-info"> 
-                        { user.name }
-                    </span>
-
-                    <NavLink
-                        className="nav-item nav-link"
-                        to="/auth/login"
-                    >
-                        iniciar sesion
-                    </NavLink>
-                    { user.logged && <button 
-                        className="nav-item nav-link btn"
-                        onClick={ handleLogout }
-                    > 
-                        Cerrar sesion
-                    </button> }
-                </ul>
-            </div>
-        </nav>
-    )
+	return (
+		<header className="header">
+			<img className="header__img" src={logo} alt="Feria de cali" />
+			<div className="header__menu--options">
+				<List
+					component="nav"
+					aria-labelledby="main navigation"
+					className={classes.navDisplayFlex} // this
+				>
+					{navLinks.map(({ title, path }) => (
+						<NavLink 
+							activeClassName="active" 
+							exact 
+							to={path} 
+							key={title} 
+							className={classes.linkText}>
+							<ListItem button>
+								<ListItemText 
+									className="textoNav"  
+									primary={title} />
+							</ListItem>
+						</NavLink>
+					))}
+				</List>
+			</div>
+			<div className="header__menu">
+				<div className="header__menu--profile">
+					<AccountCircleIcon fontSize="large"></AccountCircleIcon>					
+				</div>
+				<ul>
+					<li><a href="/">Cuenta</a></li>
+					<li><a href="/">Cerrar Sesión</a></li>
+				</ul>
+			</div>
+		</header>
+	)
 }
